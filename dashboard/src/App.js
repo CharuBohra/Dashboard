2d4ca809b27e3607f23f77d953c2fd05c9ec8e65
CharuNBohra <charu@juddoc.com>
2026-01-13 10:53:53 +0530

    COmponents and Pages Structure

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -2,6 +2,8 @@ import React,{useEffect} from 'react'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import { FiSettings } from 'react-icons/fi'
 import {TooltipComponent} from '@syncfusion/ej2-react-popups'
+import {Navbar,Sidebar,ThemeSettings,Footer} from './components'
+import {ECommerce,Orders,Calendar,Employees,Stacked,Pyramid,Customers,Kanban,Line,Area,Bar,Pie,Financial,ColorPicker,ColorMapping,Editor} from './pages'
 import './App.css'
 
 const App = () => {
